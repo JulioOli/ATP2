ddf3c0a1066dd825b02471f3b8489144849dc947
JulioOli <julio.o.santana@unesp.br>
2024-08-29 12:35:18 -0300

    não está pegando o menor ainda..

---
+++ Lista1/ex21.c
@@ -0,0 +1,35 @@
+/*Escreva um programa em C que encontre o maior e o menor
+elemento em um array de 10 inteiros.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int array[10];
+    int maior, menor;
+
+    printf("Digite 10 números inteiros\n");
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (array[i] > maior)
+        {
+            maior = array[i];
+        }
+
+        if (array[i] < menor)
+        {
+            menor = array[i];
+        }
+    }
+
+    printf("\n\no menor valor digitado é %d e o maior é %d\n\n\n", menor, maior);
+
+    return 0;
+}
