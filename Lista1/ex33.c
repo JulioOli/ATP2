50d24158fbd743f7cc4d7b9e50711bb3826df114
JulioOli <julio.o.santana@unesp.br>
2024-09-10 11:52:19 -0300

    33 usando o argv, mas eu n consegui rodar

--- Lista1/ex33.c
+++ Lista1/ex33.c
@@ -4,18 +4,25 @@ argumentos na linha de comando e imprima a soma deles.*/
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-    int soma = 0;
-    int num;
-
-    for (int i = 0; i < 3; i++)
+    // Verifica se o número de argumentos é diferente de 4
+    if (argc != 4)
     {
-        printf("Digite o %dº número inteiro: ", i + 1);
-        int num;
-        scanf("%d", &num);
-        soma += num;
+        printf("Uso: %s <num1> <num2> <num3>\n", argv[0]);
+        return 1; // Retorna com erro se a quantidade de argumentos for diferente de 4
     }
 
-    printf("\nA soma dos números é: %d\n\n", soma);
+    // Converte os argumentos de strings para inteiros
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+    int num3 = atoi(argv[3]);
+
+    // calcula a soma dos números
+    int soma = num1 + num2 + num3;
+
+    // Imprime o resultado
+    printf("%d + %d + %d é igual a %d\n", num1, num2, num3, soma);
+
+    return 0;
 }
